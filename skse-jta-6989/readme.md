fa8a140dc87e1ef2a15406864e8b4a4ff124cd55
dt <279964527@qq.com>
2021-02-05 17:50:54 +0800

    jta

---
+++ skse-jta-6989/readme.md
@@ -0,0 +1,2 @@
+# JTA，全称：Java Transaction API，管理多数据源事务  
+https://juejin.cn/post/6924940895410192397
---
+++ skse-jta-6989/src/main/java/com/skse/JTAMain6989.java
@@ -0,0 +1,19 @@
+package com.skse;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author jiangyao
+ * @Date 2021/2/5 15:20
+ **/
+@Slf4j
+@SpringBootApplication
+public class JTAMain6989 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JTAMain6989.class, args);
+        log.info("启动成功");
+    }
+}
